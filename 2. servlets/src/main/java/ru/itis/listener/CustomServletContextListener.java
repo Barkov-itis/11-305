5ea52650a4fc8422ae67afc672693977f5eeb215
danil-11906 <tron5268@mail.ru>
2024-12-21 23:59:50 +0300

    files
    added files

--- 2. servlets/src/main/java/ru/itis/listener/CustomServletContextListener.java
+++ 2. servlets/src/main/java/ru/itis/listener/CustomServletContextListener.java
@@ -1,12 +1,11 @@
 package ru.itis.listener;
 
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import ru.itis.service.SignUpService;
-import ru.itis.service.SignUpServiceImpl;
+import ru.itis.repositories.FilesRepository;
+import ru.itis.repositories.FilesRepositoryImpl;
 import ru.itis.repositories.UsersRepository;
 import ru.itis.repositories.UsersRepositoryJdbcImpl;
-import ru.itis.service.UsersService;
-import ru.itis.service.UsersServiceImpl;
+import ru.itis.services.*;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -20,11 +19,9 @@ public class CustomServletContextListener implements ServletContextListener {
     private static final String DB_PASSWORD = "gjhfqr102";
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/testdb_3";
     private static final String DB_DRIVER = "org.postgresql.Driver";
-
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         ServletContext servletContext = servletContextEvent.getServletContext();
-
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(DB_DRIVER);
         dataSource.setUsername(DB_USERNAME);
@@ -32,12 +29,14 @@ public class CustomServletContextListener implements ServletContextListener {
         dataSource.setUrl(DB_URL);
 
         UsersRepository usersRepository = new UsersRepositoryJdbcImpl(dataSource);
-        SignUpService signUpService = new SignUpServiceImpl(usersRepository);
-        UsersService usersService = new UsersServiceImpl(usersRepository);
-
         servletContext.setAttribute("usersRep", usersRepository);
+        SignUpService signUpService = new SignUpServiceImpl(usersRepository);
         servletContext.setAttribute("signUpService", signUpService);
+        UsersService usersService = new UsersServiceImpl(usersRepository);
         servletContext.setAttribute("usersService", usersService);
+        FilesRepository filesRepository = new FilesRepositoryImpl(dataSource);
+        FilesService filesService = new FilesServiceImpl(filesRepository);
+        servletContext.setAttribute("filesUploadService", filesService);
     }
 
     @Override
